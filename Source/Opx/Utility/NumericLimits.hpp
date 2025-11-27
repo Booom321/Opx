d057d5aaccb954acc6f916616dce0fb03a860933
Boom123 <baokhoi.code@gmail.com>
2025-11-27 18:30:10 +0700

    Add file

---
+++ Source/Opx/Utility/NumericLimits.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "../DataTypes.hpp"
+
+#include <limits>
+
+OPX_NAMESPACE_BEGIN
+
+namespace Details {
+    template <typename T>
+    class NumericLimitsImpl {
+    public:
+        static OPX_CONSTEXPR Bool kHasNaN = std::numeric_limits<T>::has_quiet_NaN;
+        static OPX_CONSTEXPR Bool kHasInfinity = std::numeric_limits<T>::has_infinity;
+        static OPX_CONSTEXPR Bool kIsInteger = std::numeric_limits<T>::is_integer;
+
+        static OPX_CONSTEXPR T kMin = std::numeric_limits<T>::min();
+        static OPX_CONSTEXPR T kMax = std::numeric_limits<T>::max();
+        static OPX_CONSTEXPR T kInfinity = std::numeric_limits<T>::infinity();
+        static OPX_CONSTEXPR T kNaN = std::numeric_limits<T>::quiet_NaN();
+    };
+}  // namespace Details
+
+template <typename T>
+class NumericLimits {
+    static_assert(false, "Unsupported type");
+};
+
+template <>
+class NumericLimits<bool> : public Details::NumericLimitsImpl<bool> {};
+template <>
+class NumericLimits<char> : public Details::NumericLimitsImpl<char> {};
+template <>
+class NumericLimits<signed char> : public Details::NumericLimitsImpl<signed char> {};
+template <>
+class NumericLimits<unsigned char> : public Details::NumericLimitsImpl<unsigned char> {};
+template <>
+class NumericLimits<wchar_t> : public Details::NumericLimitsImpl<wchar_t> {};
+#if defined(__cpp_char8_t)
+template <>
+class NumericLimits<char8_t> : public Details::NumericLimitsImpl<char8_t> {};
+#endif
+template <>
+class NumericLimits<char16_t> : public Details::NumericLimitsImpl<char16_t> {};
+template <>
+class NumericLimits<char32_t> : public Details::NumericLimitsImpl<char32_t> {};
+template <>
+class NumericLimits<short> : public Details::NumericLimitsImpl<short> {};
+template <>
+class NumericLimits<unsigned short> : public Details::NumericLimitsImpl<unsigned short> {};
+template <>
+class NumericLimits<int> : public Details::NumericLimitsImpl<int> {};
+template <>
+class NumericLimits<unsigned int> : public Details::NumericLimitsImpl<unsigned int> {};
+template <>
+class NumericLimits<long> : public Details::NumericLimitsImpl<long> {};
+template <>
+class NumericLimits<unsigned long> : public Details::NumericLimitsImpl<unsigned long> {};
+template <>
+class NumericLimits<long long> : public Details::NumericLimitsImpl<long long> {};
+template <>
+class NumericLimits<unsigned long long> : public Details::NumericLimitsImpl<unsigned long long> {};
+template <>
+class NumericLimits<float> : public Details::NumericLimitsImpl<float> {};
+template <>
+class NumericLimits<double> : public Details::NumericLimitsImpl<double> {};
+template <>
+class NumericLimits<long double> : public Details::NumericLimitsImpl<long double> {};
+
+OPX_NAMESPACE_END
---
+++ Test/Utility/NumericLimits.cpp
@@ -0,0 +1,35 @@
+#include "Framework/Framework.hpp"
+
+#include <Opx/Utility/NumericLimits.hpp>
+
+TEST_CASE(Utility, NumericLimits) {
+    using namespace Opx;
+
+#if defined(__cpp_char8_t)
+    TEST_EXPECT_TRUE(NumericLimits<char8_t>::kIsInteger);
+    TEST_EXPECT_TRUE(!NumericLimits<char8_t>::kHasInfinity);
+    TEST_EXPECT_TRUE(!NumericLimits<char8_t>::kHasNaN);
+#endif
+
+    TEST_EXPECT_TRUE(NumericLimits<bool>::kIsInteger && NumericLimits<char>::kIsInteger && NumericLimits<signed char>::kIsInteger &&
+                     NumericLimits<unsigned char>::kIsInteger && NumericLimits<wchar_t>::kIsInteger && NumericLimits<char16_t>::kIsInteger &&
+                     NumericLimits<char32_t>::kIsInteger && NumericLimits<unsigned short>::kIsInteger && NumericLimits<int>::kIsInteger &&
+                     NumericLimits<unsigned int>::kIsInteger && NumericLimits<long>::kIsInteger && NumericLimits<unsigned long>::kIsInteger &&
+                     NumericLimits<long>::kIsInteger && NumericLimits<unsigned long long>::kIsInteger && !NumericLimits<float>::kIsInteger &&
+                     !NumericLimits<double>::kIsInteger && !NumericLimits<long double>::kIsInteger);
+
+    TEST_EXPECT_TRUE(!NumericLimits<bool>::kHasInfinity && !NumericLimits<char>::kHasInfinity && !NumericLimits<signed char>::kHasInfinity &&
+                     !NumericLimits<unsigned char>::kHasInfinity && !NumericLimits<wchar_t>::kHasInfinity && !NumericLimits<char16_t>::kHasInfinity &&
+                     !NumericLimits<char32_t>::kHasInfinity && !NumericLimits<unsigned short>::kHasInfinity && !NumericLimits<int>::kHasInfinity &&
+                     !NumericLimits<unsigned int>::kHasInfinity && !NumericLimits<long>::kHasInfinity &&
+                     !NumericLimits<unsigned long>::kHasInfinity && !NumericLimits<long>::kHasInfinity &&
+                     !NumericLimits<unsigned long long>::kHasInfinity && NumericLimits<float>::kHasInfinity && NumericLimits<double>::kHasInfinity &&
+                     NumericLimits<long double>::kHasInfinity);
+
+    TEST_EXPECT_TRUE(!NumericLimits<bool>::kHasNaN && !NumericLimits<char>::kHasNaN && !NumericLimits<signed char>::kHasNaN &&
+                     !NumericLimits<unsigned char>::kHasNaN && !NumericLimits<wchar_t>::kHasNaN && !NumericLimits<char16_t>::kHasNaN &&
+                     !NumericLimits<char32_t>::kHasNaN && !NumericLimits<unsigned short>::kHasNaN && !NumericLimits<int>::kHasNaN &&
+                     !NumericLimits<unsigned int>::kHasNaN && !NumericLimits<long>::kHasNaN && !NumericLimits<unsigned long>::kHasNaN &&
+                     !NumericLimits<long>::kHasNaN && !NumericLimits<unsigned long long>::kHasNaN && NumericLimits<float>::kHasNaN &&
+                     NumericLimits<double>::kHasNaN && NumericLimits<long double>::kHasNaN);
+}
