bb343beee1e4dc12aabe608a601cedbb49b7727f
Boom123 <baokhoi.code@gmail.com>
2025-11-30 23:29:12 +0700

    Remove google benchmark

--- Benchmark/Main.cpp
+++ Benchmark/Main.cpp
@@ -1,13 +1,3 @@
-#include <benchmark/benchmark.h>
-
-static void BM_SomeFunction(benchmark::State& state) {
-    // Perform setup here
-    for (auto _ : state) {
-        // This code gets timed
-        int i = 0;
-    }
+int main() {
+    return 0;
 }
-// Register the function as a benchmark
-BENCHMARK(BM_SomeFunction);
-// Run the benchmark
-BENCHMARK_MAIN();
