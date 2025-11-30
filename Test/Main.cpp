bf58c08ea78a76f99a43552508769c054f410542
Boom123 <baokhoi.code@gmail.com>
2025-12-01 00:20:50 +0700

    MSVC: Implement memory leaks check

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -1,12 +1,35 @@
 #include "Framework/Framework.hpp"
+#include <Opx/Preprocessor.hpp>
 
-void Run() {
+#if defined(OPX_COMPILER_MSVC) && defined(OPX_DEBUG)
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+inline void EnableMemoryLeakDetection() {
+    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+    _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+    _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+    _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDOUT);
+    _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
+    _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDOUT);
+
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+}
+
+inline void DumpMemoryLeaks() {
+    _CrtDumpMemoryLeaks();
+}
+#else
+inline void EnableMemoryLeakDetection() {}
+inline void DumpMemoryLeaks() {}
+#endif
+
+void RunAllTests() {
     TestExecutor::Get()->Execute();
-    printf("\n");
-    system("pause");
 }
 
 int main() {
-    Run();
+    EnableMemoryLeakDetection();
+    RunAllTests();
+    DumpMemoryLeaks();
     return 0;
 }
