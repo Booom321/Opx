3b10a698639d2634a087d73e565c6bd8b1c07b61
Boom123 <baokhoi.code@gmail.com>
2025-12-05 16:31:17 +0700

    Test benchmark

--- Benchmark/Main.cpp
+++ Benchmark/Main.cpp
@@ -1,3 +1,26 @@
-int main() {
-    return 0;
+#include "Benchmark/Benchmark.hpp"
+
+void StringCreation(uint64_t iterations) {
+    for (uint64_t i = 0; i < iterations; i++) {
+        std::string s{};
+        BENCHMARK_DO_NOT_OPTIMIZE(s);
+    }
 }
+
+void StringCopy(uint64_t iterations) {
+    std::string s = "hello world!";
+    for (uint64_t i = 0; i < iterations; i++) {
+        std::string copy(s);
+        BENCHMARK_DO_NOT_OPTIMIZE(s);
+    }
+}
+
+BENCHMARK_REGISTER(Test, StringCreation, StringCreation)
+    ->SetWarmUpRuns(10000)
+    ->AddIterations(100000, 10, 4);
+
+BENCHMARK_REGISTER(Test, StringCopy, StringCopy)
+    ->SetWarmUpRuns(10000)
+    ->AddIterations(100000, 10, 4);
+
+BENCHMARK_MAIN();
