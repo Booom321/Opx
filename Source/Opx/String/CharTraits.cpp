f34c4e39856fe309c0ec8ec9a0522d9f282260ee
Boom123 <baokhoi.code@gmail.com>
2025-12-08 19:04:42 +0700

    Reduce template bloat using explicit instantiation

--- Source/Opx/String/CharTraits.cpp
+++ Source/Opx/String/CharTraits.cpp
@@ -1,6 +1,135 @@
 #include "CharTraits.hpp"
 
+#include <cstring>
+#include <cwchar>
+
 OPX_NAMESPACE_BEGIN
+
+template <typename T>
+const typename CharTraits<T>::CharType* CharTraits<T>::Copy(CharType* dest,
+                                                            const CharType* src,
+                                                            SizeT count) {
+#if defined(OPX_COMPILER_GCC) || defined(OPX_COMPILER_CLANG)
+    return static_cast<const CharType*>(__builtin_memcpy(dest, src, count * sizeof(CharType)));
+#else
+    return static_cast<const CharType*>(memcpy(dest, src, count * sizeof(CharType)));
+#endif
+}
+
+template <typename T>
+const typename CharTraits<T>::CharType* CharTraits<T>::Move(CharType* dest,
+                                                            const CharType* src,
+                                                            SizeT count) {
+    if (dest < src) {
+#if defined(OPX_COMPILER_GCC) || defined(OPX_COMPILER_CLANG)
+        return static_cast<const CharType*>(__builtin_memcpy(dest, src, count * sizeof(CharType)));
+#else
+        return static_cast<const CharType*>(memcpy(dest, src, count * sizeof(CharType)));
+#endif
+    }
+#if defined(OPX_COMPILER_GCC) || defined(OPX_COMPILER_CLANG)
+    return static_cast<const CharType*>(__builtin_memmove(dest, src, count * sizeof(CharType)));
+#else
+    return static_cast<const CharType*>(memmove(dest, src, count * sizeof(CharType)));
+#endif
+}
+
+template <typename T>
+SizeT CharTraits<T>::Length(const CharType* str) {
+    const CharType* current = str;
+    while (*current) {
+        ++current;
+    }
+    return static_cast<SizeT>(current - str);
+}
+
+template <typename T>
+Int32 CharTraits<T>::Compare(const CharType* lhs, const CharType* rhs, SizeT n) {
+    while (n > 0) {
+        if (*lhs != *rhs) {
+            return (*lhs > *rhs) ? 1 : -1;
+        }
+        ++lhs, ++rhs, --n;
+    }
+    return 0;
+}
+
+template <typename T>
+void CharTraits<T>::Fill(CharType* dest, CharType c, SizeT n) {
+    while (n > 0) {
+        *dest = c;
+        ++dest, --n;
+    }
+}
+
+template <typename T>
+const typename CharTraits<T>::CharType* CharTraits<T>::FindChar(const CharType* str,
+                                                                CharType c,
+                                                                SizeT n) {
+    while (n > 0) {
+        if (*str == c) {
+            return str;
+        }
+        ++str, --n;
+    }
+    return nullptr;
+}
+
+template <typename T>
+const typename CharTraits<T>::CharType* CharTraits<T>::FindLastChar(const CharType* str,
+                                                                    CharType c,
+                                                                    SizeT n) {
+    const CharType* end = str + n;
+    while (end != str) {
+        --end;
+        if (*end == c) {
+            return end;
+        }
+    }
+    return nullptr;
+}
+
+template <typename T>
+const typename CharTraits<T>::CharType* CharTraits<T>::FindSubstr(const CharType* str,
+                                                                  SizeT len,
+                                                                  const CharType* substr,
+                                                                  SizeT substrLen) {
+    if (substrLen > len) {
+        return nullptr;
+    }
+
+    if (substrLen == 0) {
+        return str;
+    }
+
+    for (const CharType* end = str + (len - substrLen + 1); str != end; ++str) {
+        if (*str == *substr && Compare(str, substr, substrLen) == 0) {
+            return str;
+        }
+    }
+    return nullptr;
+}
+
+template <typename T>
+const typename CharTraits<T>::CharType* CharTraits<T>::FindLastSubstr(const CharType* str,
+                                                                      SizeT len,
+                                                                      const CharType* substr,
+                                                                      SizeT substrLen) {
+    if (substrLen > len) {
+        return nullptr;
+    }
+
+    if (substrLen == 0) {
+        return str + len;
+    }
+
+    for (const CharType* end = str + (len - substrLen); end != str; --end) {
+        if (*end == *substr && Compare(end, substr, substrLen) == 0) {
+            return end;
+        }
+    }
+    return nullptr;
+}
 
 /////// CHAR ///////
 template <>
@@ -75,5 +204,13 @@ const typename CharTraits<WChar>::CharType* CharTraits<WChar>::FindChar(const Ch
     return wmemchr(str, c, n);
 #endif
 }
+
+template class CharTraits<Char>;
+template class CharTraits<WChar>;
+#if defined(OPX_HAS_CHAR18_T)
+template class CharTraits<Char8>;
+#endif
+template class CharTraits<Char16>;
+template class CharTraits<Char32>;
 
 OPX_NAMESPACE_END
--- Source/Opx/String/CharTraits.hpp
+++ Source/Opx/String/CharTraits.hpp
@@ -1,127 +1,32 @@
 #pragma once
 
 #include "../DataTypes.hpp"
-
-#include <cstring>
-#include <cwchar>
-#include <iostream>
+#include "../TypeTraits.hpp"
 
 OPX_NAMESPACE_BEGIN
 
 template <typename T>
 class CharTraits {
 public:
+    static_assert(IsCharacter_V<T>, "T must be a character type.");
+
     using CharType = T;
 
-    static const CharType* Copy(CharType* dest, const CharType* src, SizeT count) {
-#if defined(OPX_COMPILER_GCC) || defined(OPX_COMPILER_CLANG)
-        return static_cast<const CharType*>(__builtin_memcpy(dest, src, count * sizeof(CharType)));
-#else
-        return static_cast<const CharType*>(memcpy(dest, src, count * sizeof(CharType)));
-#endif
-    }
-
-    static const CharType* Move(CharType* dest, const CharType* src, SizeT count) {
-        if (dest < src) {
-#if defined(OPX_COMPILER_GCC) || defined(OPX_COMPILER_CLANG)
-            return static_cast<const CharType*>(
-                __builtin_memcpy(dest, src, count * sizeof(CharType)));
-#else
-            return static_cast<const CharType*>(memcpy(dest, src, count * sizeof(CharType)));
-#endif
-        }
-#if defined(OPX_COMPILER_GCC) || defined(OPX_COMPILER_CLANG)
-        return static_cast<const CharType*>(__builtin_memmove(dest, src, count * sizeof(CharType)));
-#else
-        return static_cast<const CharType*>(memmove(dest, src, count * sizeof(CharType)));
-#endif
-    }
-
-    static SizeT Length(const CharType* str) {
-        const CharType* current = str;
-        while (*current) {
-            ++current;
-        }
-        return static_cast<SizeT>(current - str);
-    }
-
-    static Int32 Compare(const CharType* lhs, const CharType* rhs, SizeT n) {
-        while (n > 0) {
-            if (*lhs != *rhs) {
-                return (*lhs > *rhs) ? 1 : -1;
-            }
-            ++lhs, ++rhs, --n;
-        }
-        return 0;
-    }
-
-    static void Fill(CharType* dest, CharType c, SizeT n) {
-        while (n > 0) {
-            *dest = c;
-            ++dest, --n;
-        }
-    }
-
-    static const CharType* FindChar(const CharType* str, CharType c, SizeT n) {
-        while (n > 0) {
-            if (*str == c) {
-                return str;
-            }
-            ++str, --n;
-        }
-        return nullptr;
-    }
-
-    static const CharType* FindLastChar(const CharType* str, CharType c, SizeT n) {
-        const CharType* end = str + n;
-        while (end != str) {
-            --end;
-            if (*end == c) {
-                return end;
-            }
-        }
-        return nullptr;
-    }
-
+    static const CharType* Copy(CharType* dest, const CharType* src, SizeT count);
+    static const CharType* Move(CharType* dest, const CharType* src, SizeT count);
+    static SizeT Length(const CharType* str);
+    static Int32 Compare(const CharType* lhs, const CharType* rhs, SizeT n);
+    static void Fill(CharType* dest, CharType c, SizeT n);
+    static const CharType* FindChar(const CharType* str, CharType c, SizeT n);
+    static const CharType* FindLastChar(const CharType* str, CharType c, SizeT n);
     static const CharType* FindSubstr(const CharType* str,
                                       SizeT len,
                                       const CharType* substr,
-                                      SizeT substrLen) {
-        if (substrLen > len) {
-            return nullptr;
-        }
-
-        if (substrLen == 0) {
-            return str;
-        }
-
-        for (const CharType* end = str + (len - substrLen + 1); str != end; ++str) {
-            if (*str == *substr && Compare(str, substr, substrLen) == 0) {
-                return str;
-            }
-        }
-        return nullptr;
-    }
-
+                                      SizeT substrLen);
     static const CharType* FindLastSubstr(const CharType* str,
                                           SizeT len,
                                           const CharType* substr,
-                                          SizeT substrLen) {
-        if (substrLen > len) {
-            return nullptr;
-        }
-
-        if (substrLen == 0) {
-            return str + len;
-        }
-
-        for (const CharType* end = str + (len - substrLen); end != str; --end) {
-            if (*end == *substr && Compare(end, substr, substrLen) == 0) {
-                return end;
-            }
-        }
-        return nullptr;
-    }
+                                          SizeT substrLen);
 };
 
 OPX_NAMESPACE_END
