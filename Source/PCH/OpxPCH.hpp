7ce9d3f60837d7f9634d7058cce9efaf7f939ac1
Boom123 <baokhoi.code@gmail.com>
2025-12-10 17:34:21 +0700

    Update file

--- Source/PCH/OpxPCH.hpp
+++ Source/PCH/OpxPCH.hpp
@@ -3,9 +3,15 @@
 #include <stb_sprintf.h>
 
 #include <cstdio>
+#include <cwchar>
+#include <cstring>
 #include <stdlib.h>
 
 #include "../Opx/Assert.hpp"
 #include "../Opx/DataTypes.hpp"
 #include "../Opx/Preprocessor.hpp"
 #include "../Opx/TypeTraits.hpp"
+
+#include "../Opx/Containers/Array.hpp"
+#include "../Opx/Containers/Vector.hpp"
+#include "../Opx/String/String.hpp"
