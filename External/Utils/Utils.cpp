d42db11570a683275062e45e49d51c634823b99b
Booom321 <132348408+Booom321@users.noreply.github.com>
2025-12-27 11:52:00 +0700

    Initial commit

---
+++ External/Utils/Utils.cpp
@@ -0,0 +1,9 @@
+#include "Utils.hpp"
+
+#include <iostream>
+
+namespace Utils {
+    void Hello() {
+        std::cout << "Hello world\n";
+    }
+}  // namespace Utils
---
+++ External/Utils/Utils.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace Utils {
+    UTILS_API void Hello();
+}
